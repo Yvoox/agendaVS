72e6436ee82e0d8ccb17e17e30a7cc2ffd139e16
Yvoox <alexandre-colicchio@hotmail.fr>
2018-01-14 13:22:26 +0100

    Let implement events

---
+++ agenda/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace agenda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.fichierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fichierToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.editionToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.eventDrawer1 = new agenda.EventDrawer();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.agendaDrawer1 = new agenda.AgendaDrawer();
+            this.contextMenuStrip2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.eventDrawer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fichierToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(122, 28);
+            // 
+            // fichierToolStripMenuItem
+            // 
+            this.fichierToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editionToolStripMenuItem});
+            this.fichierToolStripMenuItem.Name = "fichierToolStripMenuItem";
+            this.fichierToolStripMenuItem.Size = new System.Drawing.Size(121, 24);
+            this.fichierToolStripMenuItem.Text = "Fichier";
+            // 
+            // editionToolStripMenuItem
+            // 
+            this.editionToolStripMenuItem.Name = "editionToolStripMenuItem";
+            this.editionToolStripMenuItem.Size = new System.Drawing.Size(131, 26);
+            this.editionToolStripMenuItem.Text = "Edition";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fichierToolStripMenuItem1,
+            this.editionToolStripMenuItem1,
+            this.quitterToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1112, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fichierToolStripMenuItem1
+            // 
+            this.fichierToolStripMenuItem1.Name = "fichierToolStripMenuItem1";
+            this.fichierToolStripMenuItem1.Size = new System.Drawing.Size(64, 24);
+            this.fichierToolStripMenuItem1.Text = "Fichier";
+            // 
+            // editionToolStripMenuItem1
+            // 
+            this.editionToolStripMenuItem1.Name = "editionToolStripMenuItem1";
+            this.editionToolStripMenuItem1.Size = new System.Drawing.Size(68, 24);
+            this.editionToolStripMenuItem1.Text = "Edition";
+            // 
+            // quitterToolStripMenuItem
+            // 
+            this.quitterToolStripMenuItem.Name = "quitterToolStripMenuItem";
+            this.quitterToolStripMenuItem.Size = new System.Drawing.Size(67, 24);
+            this.quitterToolStripMenuItem.Text = "Quitter";
+            this.quitterToolStripMenuItem.Click += new System.EventHandler(this.quitterToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.Controls.Add(this.eventDrawer1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(912, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 567);
+            this.panel1.TabIndex = 3;
+            // 
+            // eventDrawer1
+            // 
+            this.eventDrawer1.Controls.Add(this.label5);
+            this.eventDrawer1.Controls.Add(this.button1);
+            this.eventDrawer1.Controls.Add(this.textBox4);
+            this.eventDrawer1.Controls.Add(this.label4);
+            this.eventDrawer1.Controls.Add(this.textBox3);
+            this.eventDrawer1.Controls.Add(this.label3);
+            this.eventDrawer1.Controls.Add(this.textBox2);
+            this.eventDrawer1.Controls.Add(this.label2);
+            this.eventDrawer1.Controls.Add(this.textBox1);
+            this.eventDrawer1.Controls.Add(this.label1);
+            this.eventDrawer1.Location = new System.Drawing.Point(0, 3);
+            this.eventDrawer1.Name = "eventDrawer1";
+            this.eventDrawer1.Size = new System.Drawing.Size(200, 564);
+            this.eventDrawer1.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(24, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Mes événements";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 473);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 30);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Ajouter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(27, 363);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(147, 22);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(78, 330);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Lieu";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(27, 281);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(147, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Titre";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(27, 199);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(147, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Heure fin";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(27, 120);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(147, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Heure début";
+            // 
+            // agendaDrawer1
+            // 
+            this.agendaDrawer1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.agendaDrawer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.agendaDrawer1.Location = new System.Drawing.Point(0, 28);
+            this.agendaDrawer1.Name = "agendaDrawer1";
+            this.agendaDrawer1.Size = new System.Drawing.Size(912, 567);
+            this.agendaDrawer1.TabIndex = 4;
+            this.agendaDrawer1.Paint += new System.Windows.Forms.PaintEventHandler(this.agendaDrawer1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1112, 595);
+            this.Controls.Add(this.agendaDrawer1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Agenda";
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.eventDrawer1.ResumeLayout(false);
+            this.eventDrawer1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem fichierToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editionToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fichierToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem editionToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem quitterToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private AgendaDrawer agendaDrawer1;
+        private EventDrawer eventDrawer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private EventManager eventManager;
+    }
+}
+
---
+++ agenda/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace agenda
+{
+    public partial class Form1 : Form
+    {
+
+        
+        
+        public Form1()
+        {
+
+            eventManager = new EventManager();
+            InitializeComponent();
+
+
+
+        }
+
+        private void quitterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void agendaDrawer1_Paint(object sender, PaintEventArgs e)
+        {
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            eventManager.addEvent(new Event(agendaDrawer1.ReturnStartTime(), agendaDrawer1.ReturnStopTime(), textBox3.Text, textBox4.Text));
+            MessageBox.Show("Evenement créé avec succès !", "My agenda");
+            agendaDrawer1.UpdateEManager(eventManager);
+            agendaDrawer1.Invalidate();
+        }
+
+
+
+        public void UpdateTime()
+        {
+           
+            textBox1.Text = agendaDrawer1.ReturnStartTime().ToString("HH:mm");
+            textBox2.Text = agendaDrawer1.ReturnStopTime().ToString("HH:mm");
+            eventDrawer1.Invalidate();
+        }
+
+
+    }
+
+    public class AgendaDrawer : Panel
+    {
+        private int dragX = 0;
+        private int dragY = 0;
+        private int dragWidth = 0;
+        private int dragHeight = 0;
+        private bool isClick = false;
+        private int posClickX = 0;
+        private int posClickY = 0;
+
+        private int memX1 = 0;
+        private int memX2 = 0;
+
+        DateTime dateStart;
+        DateTime dateStop;
+
+        Rectangle sel = new Rectangle();
+
+        EventManager eManager;
+
+
+        public AgendaDrawer()
+        {
+
+            this.Paint += AgendaDrawer_Paint;
+            this.MouseDown += AgendaDrawer_MouseDown;
+            this.MouseUp += AgendaDrawer_MouseUp;
+            this.MouseMove += AgendaDrawer_MouseMove;
+            this.DoubleBuffered = true;
+            eManager = new EventManager();
+
+
+        }
+
+        private void AgendaDrawer_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isClick)
+            {
+                sel.Height = e.Y - posClickY;
+                sel.Width = e.X - posClickX;
+                this.Invalidate();
+                
+            }
+        }
+
+        private void AgendaDrawer_MouseUp(object sender, MouseEventArgs e)
+        {
+            memX1 = posClickY;
+            memX2 =  sel.Height;
+            sel.X = 0;
+            sel.Y = 0;
+            sel.Height = 0;
+            sel.Width = 0;
+            posClickX = 0;
+            posClickY = 0;
+            isClick = false;
+            
+
+
+
+            (this.Parent as Form1).UpdateTime();
+
+            this.Invalidate();
+            
+        }
+
+        private void AgendaDrawer_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            sel.X = e.X;
+            sel.Y = e.Y;
+            sel.Height = dragHeight;
+            sel.Width = dragWidth;
+            posClickX = e.X;
+            posClickY = e.Y;
+            isClick = true;
+
+            this.Invalidate();
+
+        }
+
+        private void AgendaDrawer_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+
+            Font drawFont = new Font("Arial", 8);
+            SolidBrush drawBrush = new SolidBrush(Color.LightGray);
+            SolidBrush rectBrush = new SolidBrush(Color.LightGreen);
+            double heure = 8.0;
+
+            DateTime dateTime = new DateTime(2017, 1, 1, 0, 0, 0);
+            dateTime = dateTime.AddHours(heure);
+
+            graphics.FillRectangle(rectBrush, sel);
+
+            for (int i =1; i<=11; i++)
+            {
+                graphics.DrawString(dateTime.ToString("HH:mm"), drawFont, drawBrush, new Point(15, i * (this.Size.Height / 11) - 6));
+                graphics.DrawLine(Pens.LightGray, new Point(17+(int)graphics.MeasureString(dateTime.ToString("HH:mm"),drawFont).Width, i * (this.Size.Height / 11)), new Point(this.Size.Width - (int)graphics.MeasureString(dateTime.ToString("HH:mm"), drawFont).Width, i * (this.Size.Height / 11)));
+                dateTime = dateTime.AddHours(1);
+            }
+
+            
+
+        }
+
+        public DateTime ReturnStartTime()
+        {
+            dateStart = new DateTime(2017, 1, 1, 0, 0, 0);
+
+            for (int i = 0; i <= 11; i++)
+            {
+                 if(i * (this.Size.Height / 11) > memX1 && i+1 * (this.Size.Height / 11) < memX1)
+                {
+                    if (i != 1) {dateStart = dateStart.AddHours(8+(double)i-2); }
+                    
+                    break;
+                }
+            }
+
+            return dateStart;
+        }
+
+        public DateTime ReturnStopTime()
+        {
+            int nbHour = memX2 / (this.Size.Height / 11);
+            dateStop = dateStart;
+            dateStop = dateStop.AddHours((double)nbHour+1);
+
+            return dateStop;
+        }
+
+        public void UpdateEManager(EventManager eventManager)
+        {
+            eManager = eventManager;
+        }
+    }
+
+    public class EventDrawer : Panel
+    {
+       
+        private DateTime startTime;
+        private DateTime stopTime;
+
+        public EventDrawer()
+        {
+            this.Paint += EventDrawer_Paint;
+            startTime = new DateTime(2017, 1, 1, 0, 0, 0);
+        }
+
+        private void EventDrawer_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            
+
+            Font drawFont = new Font("Arial", 8);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+
+            
+        }
+
+        public void SetStartTime(DateTime time)
+        {
+            startTime = time;
+        }
+
+        public void SetStopTime(DateTime time)
+        {
+            stopTime = time;
+        }
+
+    }
+
+    public class Event
+    {
+        private DateTime startTime = new DateTime(2017, 1, 1, 0, 0, 0);
+        private DateTime stopTime = new DateTime(2017, 1, 1, 0, 0, 0);
+        private String title;
+        private String place;
+
+        public Event(DateTime start, DateTime stop, String title, String place)
+        {
+            startTime = start;
+            stopTime = stop;
+            this.title = title;
+            this.place = place;
+        }
+
+        public Event(DateTime start, DateTime stop, String place)
+        {
+            startTime = start;
+            stopTime = stop;
+            this.title = "";
+            this.place = place;
+        }
+
+        public DateTime StartTime { get => startTime; set => startTime = value; }
+        public DateTime StopTime { get => stopTime; set => stopTime = value; }
+        public string Title { get => title; set => title = value; }
+        public string Place { get => place; set => place = value; }
+    }
+
+    public class EventManager
+    {
+        private List<Event> eventList;
+
+        public EventManager()
+        {
+            eventList = new List<Event>();
+        }
+
+        public void addEvent(Event e)
+        {
+            eventList.Add(e);
+        }
+
+        public void removeEvent(String title)
+        {
+            
+        }
+
+        public List<Event> returnEvents()
+        {
+            return eventList;
+        }
+
+        public int eSize()
+        {
+           return eventList.Count();
+        }
+    }
+
+
+}
